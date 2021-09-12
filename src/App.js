80f74ab81ca1e2d334f0970f498a68e2896e1fd8
songha <thdgk6304@naver.com>
2021-09-12 16:21:34 +0900

    9강 조건 렌더링

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import Counter from './components/Counter';
 
 function App() {
-  const [buttonName, setButtonName] = useState('클릭');
-  const clickButton = () => {
-    setButtonName('click');
-  };
-  return (    
+  const [condition, setCondition] = useState(false);
+  const toggle = () => setCondition(!condition);
+
+  useEffect(() => {
+    console.log(condition);
+  }, [condition])
+  const renderCondition = condition
+    ? 'True'
+    : 'False'
+  return (
     <div className="App">
       <h1>Kossie Coder</h1>
-      <Counter click="Click1" />
-      <Counter click={buttonName}/>
-      <Counter />
-      <button onClick={clickButton}>Click</button>
+      <div>
+        {renderCondition}
+      </div>     
+      <button onClick={toggle}>Toggle</button>
     </div>
   );
 }
