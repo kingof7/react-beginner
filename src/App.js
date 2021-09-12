2b6d2295b6b3dd5f57cdcdb11659a6eb3aec1f2f
jhlee <vlfl0@naver.com>
2021-09-12 13:12:52 +0900

    initial commit : useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  //const [text, setText] = useState('Kossie'); //담을값,업데이트함수
+  const onSubmit = (event) => {
+    alert('로그인에 성공하셨습니다.');
+    event.preventDefault(); // refresh 막아줌
+    console.log(username, password)
+  }
+
+  // const onKeyUp = (event) => {
+  //   if (event.keyCode === 13){ //enter key
+  //     console.log(event.keyCode);
+  //     onSubmit();
+  //   }
+  // }
+
+  //let text = "Kossie";
+
+  // const updateText = () => {
+  //   //text = 'Coder';
+  //   setText('Coder');
+  //   console.log(text);
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input 
+        placeholder="Username" 
+        value={username}
+        onChange={e => setUsername(e.target.value)} /><br />
+        <input 
+        placeholder="Password" 
+        value={password}
+        onChange={e => setPassword(e.target.value)}/><br />
+        <button type="submit">Login</button>
+      </form>   
     </div>
   );
 }
