5314e1818632a4577c85cda8cc4015e1193637cb
songha <thdgk6304@naver.com>
2021-09-12 16:35:59 +0900

    10강 리액트 반복문

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 function App() {
-  const [condition, setCondition] = useState(false);
-  const toggle = () => setCondition(!condition);
+  const movies = [ //array
+    { title: 'kossie coder1', year: 2001 }, //object
+    { title: 'kossie coder2', year: 2003 },
+    { title: 'kossie coder3', year: 2005 },
+    { title: 'kossie coder4', year: 2010 },
+  ];
 
-  useEffect(() => {
-    console.log(condition);
-  }, [condition])
-  const renderCondition = condition
-    ? 'True'
-    : 'False'
+  const renderMovies = movies.map(movie => {
+    return (
+      <div className="movie" key={movie.title}>
+        <div className="movie-title">{movie.title}</div>
+        <div className="movie-year">{movie.year}</div>
+      </div>
+    );
+  }); //map을 쓰면 array에서 아이템 하나하나 가져옴
+
   return (
     <div className="App">
-      <h1>Kossie Coder</h1>
-      <div>
-        {renderCondition}
-      </div>     
-      <button onClick={toggle}>Toggle</button>
+      <h1>Movie list</h1>
+      {renderMovies}
+      {/* <div className="movie">
+        <div className="movie-title">{movies[0].title}</div>
+        <div className="movie-year">{movies[0].year}</div>
+      </div>
+      <div className="movie">
+        <div className="movie-title">{movies[1].title}</div>
+        <div className="movie-year">{movies[1].year}</div>
+      </div>
+      <div className="movie">
+        <div className="movie-title">{movies[2].title}</div>
+        <div className="movie-year">{movies[2].year}</div>
+      </div> */}
     </div>
   );
 }
