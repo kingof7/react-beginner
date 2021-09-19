84cbffe4a1aecb648e41de468e9c90671c73ce1a
jhlee <jhlee@sorin.co.kr>
2021-09-19 19:39:21 +0900

    17 InputField 컴포넌트로 빼내기 & Fragment

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import InputField from './InputField';
 
 const MovieForm = ({ addMovie }) => {
     const [movieTitle, setMovieTitle] = useState('');
@@ -46,20 +47,34 @@ const MovieForm = ({ addMovie }) => {
 
     return (  
         <form onSubmit={onSubmit}>
-            <input
+            <InputField 
+                type="text"
+                value={movieTitle}
+                placeholder="영화제목"
+                onChange={e => setMovieTitle(e.target.value)}
+                errorMessage={titleError}
+            />
+            {/* <input
                 type="text"
                 value={movieTitle}
                 placeholder="영화제목"
                 onChange={e => setMovieTitle(e.target.value)}
             /> <br />
-            <div style={{color: 'red'}}>{titleError}</div>
-            <input
+            <div style={{color: 'red'}}>{titleError}</div> */}
+            <InputField 
+                type="number"
+                value={movieYear}
+                placeholder="개봉년도"
+                onChange={e => setMovieYear(e.target.value)}
+                errorMessage={yearError}
+            />
+            {/* <input
                 type="number"
                 value={movieYear}
                 placeholder="개봉년도"
                 onChange={e => setMovieYear(e.target.value)}
             /> <br />
-            <div style={{color: 'red'}}>{yearError}</div>
+            <div style={{color: 'red'}}>{yearError}</div> */}
             <button type="submit">영화추가</button>
         </form>
     );
