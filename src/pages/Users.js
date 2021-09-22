d0035900f18cec7dbd10ca6217a3c233520c0fba
jhlee <jhlee@sorin.co.kr>
2021-09-22 16:08:18 +0900

    26 로딩 Spinner 추가하기

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import UserList from '../components/UserList';
+import Spinner from '../components/Spinner';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => {
                 setUsers(response.data);
+                setLoading(false);
             });
     }, []);
 
@@ -18,7 +22,8 @@ const Users = () => {
     return (
         <>
           <h1>Users</h1>
-          <UserList users={users} />
+          {loading ? <Spinner /> : 
+            <UserList users={users} />}
         </>
     );
 };
