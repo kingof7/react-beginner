95ed16777637eb0b87de02d3d451142c10fea040
jhlee <jhlee@sorin.co.kr>
2021-09-22 17:26:39 +0900

    29 30 (마지막) user 페이지

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UserList = ({ users }) => {
     console.log(users);
     return (
-        <div>
-            
+        <div>            
             {users.map(user => {
-                return (
-                    <div className="card mb-3" key={user.id}>
+                return(                               
+                    <div 
+                        className="card mb-3"
+                        key={user.id}
+                    >
                         <div className="card-body p-3">
-                            {user.name}
+                            <Link to={`/users/${user.id}`}>{user.name}</Link>
                         </div>
                     </div>
-                );
-            })}
+                );              
+            
+            })};
         </div>
     );
 };
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import Users from './pages/Users';
+import User from './pages/User';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
 
@@ -15,5 +16,9 @@ export default [
     {
         path: '/users',
         component: Users
+    },
+    {
+        path: '/users/:id',
+        component: User
     },
 ];
